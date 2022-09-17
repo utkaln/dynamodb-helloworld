a9b1a0749494fd088f4ecf75411001d65f0d5581
Utkal Nayak <utkalnayak@Utkals-MacBook-Pro.local>
2022-09-16 21:09:51 -0400

    added async js

---
+++ src/js/asyncjs.js
@@ -0,0 +1,44 @@
+// practice call back
+function addNumbers(a,b, callback){
+    callback(a*b);
+}
+console.log ("calling callback method");
+addNumbers(50000,70, (c) => console.log("Value of C " + c));
+
+
+// practice promise
+function addNumbersPromise(a,b){
+    return new Promise((resolve, reject) => {
+        let successCode = {
+            responseCode: 200,
+            message: "Success",
+            value: (a+b)
+        };
+
+        let errorCode = {
+            responseCode: 400,
+            message: "Error",
+            value: null
+        };
+        if (typeof a === 'number' && typeof b === 'number'){
+            resolve(successCode);
+        } else {
+            errorCode.value = "Input params not number";
+            reject(errorCode);
+        }
+    });
+}
+
+console.log ("calling promise method");
+addNumbersPromise(50,'abc').then(
+    (successCode) => {
+        console.log(successCode);
+    },
+    (errorCode) => {
+        console.log(errorCode);
+    }
+);
+
+
+// chaining promises
+console.log ("calling chaining promise method");
